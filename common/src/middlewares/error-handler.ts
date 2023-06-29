52e6061494a4fde2057d969d66f91ef532b2fdf3
Siva M <Siva.Marimuthu@tesco.com>
2023-06-29 22:38:46 +0530

    updates

--- common/src/middlewares/error-handler.ts
+++ common/src/middlewares/error-handler.ts
@@ -1,13 +1,13 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { CustomError } from "../errors/CustomError";
 
-export const handleErrors = (err: Error, req: Request, res: Response) => {
+export const handleErrors = (err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.log(err);
   if (err instanceof CustomError) {
     return res.status(err.statuscode).jsonp({
       errors: err.serializeErrors()
     });
   }
-  console.log(err);
   return res.status(400).jsonp({
     errors: [{ message: "Something went wrong!" }]
   });
